fcef3a468ac18347e9d02172f57f4870dcd23a46
Kevin Macaulay <kevin@softwareops.com>
2015-05-20 15:51:36 -0700

    Updating the SDK and Objective-C demo app, adding the Swift demo app.

---
+++ AclipsaSDK/AclipsaSDK.framework/Headers/AclipsaSDK.h
@@ -0,0 +1,25 @@
+//
+//  AclipsaSDK.h
+//  AclipsaSDK
+//
+//  Created by Kevin Macaulay on 5/4/15.
+//  Copyright (c) 2015 Aclipsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <AclipsaSDK/ACLIPConstants.h>
+#import <AclipsaSDK/ACLIPMoviePlayerViewController.h>
+#import <AclipsaSDK/ACLIPMoviePlayerController.h>
+#import <AclipsaSDK/ACLIPSession.h>
+#import <AclipsaSDK/ACLIPUtilities.h>
+#import <AclipsaSDK/ACLIPMessage.h>
+#import <AclipsaSDK/ACLIPMessageThread.h>
+#import <AclipsaSDK/ACLIPUser.h>
+#import <AclipsaSDK/ACLIPVideo.h>
+#import <AclipsaSDK/ACLIPMessageStatus.h>
+#import <AclipsaSDK/UIImage+ACLIPAdditions.h>
+#import <AclipsaSDK/UIImageView+ACLIPAdditions.h>
+#import <AclipsaSDK/UIButton+ACLIPAdditions.h>
+#import <AclipsaSDK/AFNetworking.h>
+#import <AclipsaSDK/UIKit+AFNetworking.h>
